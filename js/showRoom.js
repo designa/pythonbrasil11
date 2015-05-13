c1be7ece51a1fa6992b4fed1bc51abfadffd3119
Michel Allan <michel_allan20xx@hotmail.com>
2015-05-13 17:17:59 -0300

    fix to showRoom js

--- js/showRoom.js
+++ js/showRoom.js
@@ -2,7 +2,9 @@ $(function(){
   showRoom();
 
   function showRoom(){
-    $(".days a").on("click", function(){
+    $(".days a").on("click", function(e){
+      e.preventDefault();
+
       var dayId = $(this).attr("href");
 
       $("*").removeClass("active");
